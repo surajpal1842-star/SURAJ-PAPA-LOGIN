2160a1daa5c07093665c5cb043d71df7116c6bea
surajpal1842-star <surajpal1842@gmail.com>
2026-09-12 02:42:19 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCajK7dqg2SS-FRSbJHxonQQxr49NF5vZ8",
+    authDomain: "fuck-c46e9.firebaseapp.com",
+    databaseURL: "https://fuck-c46e9-default-rtdb.firebaseio.com",
+    projectId: "fuck-c46e9",
+    storageBucket: "fuck-c46e9.firebasestorage.app",
+    messagingSenderId: "1043993934415",
+    appId: "1:1043993934415:android:30ebdcdc70ca8cbf88083a"
+};
